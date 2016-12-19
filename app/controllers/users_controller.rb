57a6890637f84520d0dc67692fb0d4861204ac42
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2016-12-19 12:28:15 -0800

    completed user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to root_path
+    login(@user)
+    redirect_to @user
   end
 
   def show
     @user = User.find(params[:id])
   end
+
 
   private
 
